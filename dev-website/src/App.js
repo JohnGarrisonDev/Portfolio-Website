eb9373c8d1f05715d4233c43fa19cc76154a2269
John Garrison <johngarrison1996@gmail.com>
2020-05-06 16:34:13 -0500

    first commit

---
+++ dev-website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Todos from './components/Todos'
+function App() {
+  return (
+    <div className="App">
+    <h1> Hello World </h1>
+    <p>{Todos}</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ dev-website/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Todos extends Component{
+    render(){
+        return (
+            <div className="Todos">
+            <h1> This is a Todo Item </h1>
+            </div>
+          );
+        }
+}
+
+export default Todos;
